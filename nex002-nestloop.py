4a58b146eac33e8dcb3aa9e93d408e08a671137c
abrief <cedek01@gmail.com>
2024-03-08 08:18:42 +0900

    Add files via upload

---
+++ nex002-nestloop.py
@@ -0,0 +1,15 @@
+def swap(data, a, b):
+    temp = data[a]
+    data[a] = data[b]
+    data[b] = temp
+
+data = [6, 5, 4, 3, 2, 1]
+
+for i in range(1, len(data)):
+    print(i)
+    for j in range(1, len(data)):
+        print(i, j)
+        if(data[j] < data[j-1]):
+            swap(data, j, j-1)
+
+print(data)
